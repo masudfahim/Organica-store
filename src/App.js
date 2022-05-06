65e9c083053288fbddfa2deb48a38109b0f5bb65
masudfahim <masudparvezofficial@gmail.com>
2022-05-06 21:17:02 +0600

    authentication done

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import Home from './Components/Pages/Home/Home/Home';
 import Product from './Components/Pages/Home/Product/Product';
 import Products from './Components/Pages/Home/Products/Products';
 import ProductDetail from './Components/Pages/Home/ProductsDetail/ProductDetail';
+import Login from './Components/Pages/Login/Login/Login';
+import Register from './Components/Pages/Login/Register/Register';
+import SocialLogin from './Components/Pages/Login/SocialLogin/SocialLogin';
 import Footer from './Components/Shared/Footer/Footer';
 import Header from './Components/Shared/Header/Header';
+import Loading from './Components/Shared/Loading/Loading';
 
 function App() {
   return (
@@ -24,6 +28,11 @@ function App() {
         <Route path='/bannar' element={<Bannar></Bannar>}></Route>
         <Route path='/products' element={<Products></Products>}></Route>
         <Route path='/product' element={<Product></Product>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/sociallogin' element={<SocialLogin></SocialLogin>}></Route>
+        <Route path='/loading' element={<Loading></Loading>}></Route>
+
       </Routes>
       <Footer></Footer>
 
--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,8 +1,18 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar, } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Header = () => {
+
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
+
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
@@ -16,10 +26,14 @@ const Header = () => {
 
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Dank memes
-                            </Nav.Link>
+                            <Nav.Link as={Link} to="about">About</Nav.Link>
+                            {
+                                user ?
+                                    <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>sign out</button>
+                                    :
+                                    <Nav.Link as={Link} to="login">
+                                        Login
+                                    </Nav.Link>}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
